816e7c18f789e04f21595268cb7a2ac73ceb6e5f
Eric Tetz <EricTetz@users.noreply.github.com>
2023-12-26 16:01:06 -0700

    calculate power dice programmatically

--- dice.mjs
+++ dice.mjs
@@ -2,29 +2,32 @@ function roll(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-const rollOptions = {
-	['2']:  ["d2"],
-	['4']:  ["d4"],
-	['6']:  ["d6"],
-	['8']:  ["d8"],
-	['10']: ["d8 + d2", "d6 + d4"],
-	['12']: ["2d6", "d8 + d4"],
-	['14']: ["d8 + d6"],
-	['16']: ["2d8"],
-	['18']: ["2d8 + d2", "d8 + d6 + d4", "2d6 + d2"],
-	['20']: ["2d8 + d4", "d8 + 2d6"],
-	['22']: ["2d8 + d6", "3d6 + d4"],
-	['24']: ["3d8"],
-	['26']: ["3d8 + d2", "3d6 + d8"],
+export function PWR(power) {
+	power += 2;
+
+    let result = [];
+    let d8s = Math.floor(power / 8);
+    let d6s = 0;
+
+    if (d8s < 1) {
+        return 'd' + power % 8;
+    } else if (power == 10) {
+        return 'd6 + d4';
+    } else if (power % 8 == 6) {
+        d6s = 1;
+    } else if (power % 8 == 4) {
+        d6s = 2;
+    } else if (power % 8 == 2) {
+        d6s = 3;
+    }
+    d8s = Math.floor((power - d6s*6) / 8);
+
+    if (d8s > 0) { result.push(d8s > 1 ? `${d8s}d8` : 'd8'); }
+    if (d6s > 0) { result.push(d6s > 1 ? `${d6s}d6` : 'd6'); }
+
+    return result.join(' + ');
 }
 
-export function PWR(pwr) {
-	let options = rollOptions[pwr *2];
-	if (options == null) {
-		throw new Error(`Power higher than ${pwr} not supported.`);
-	}
-	return options;
-}
 
 export function check() {
 	return (roll(1, 12) - roll(1, 12));
